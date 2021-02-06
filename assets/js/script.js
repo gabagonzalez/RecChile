43857d0a50b1f4d6ea178e3c2e4f66130b948156
gaba gonzalez <gaba2801@gmail.com>
2021-02-06 08:19:01 -0300

    img, color, text

---
+++ assets/js/script.js
@@ -0,0 +1,44 @@
+
+//funcion scroll
+$(function(){
+	$("a").click(function(event) {
+		if (this.hash !== "") {
+			event.preventDefault();
+
+			var gato = this.hash;
+
+			$('html, body').animate({
+			scrollTop: $(gato).offset().top
+			}, 1200, function(){
+			window.location.hash = gato;
+		});
+		} 
+	});
+});
+
+//funcion popover
+$(function () {
+    $('[data-toggle="popover"]').popover()
+    
+	})
+	
+// Funtion Animate in Navbar-Site
+//and Active Tooltip
+$(function(){
+    $("a").click(function(event){
+       if (this.hash !== "") {
+           event.preventDefault();
+
+           var gato = this.hash;
+
+           $("html, body").animate({
+               scrollTop: $(gato).offset().top
+           }, 1200, function(){
+               window.location.hash = gato;
+           });
+       }
+   });
+   
+   $('[data-toggle="tooltip"]').tooltip();
+
+});
